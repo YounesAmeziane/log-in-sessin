c151f320463302baf04b329325c71c5a1be3f6b0
Younes Ameziane <73488317+YounesAmeziane@users.noreply.github.com>
2021-09-13 17:57:57 +0100

    Create logout-function.php

---
+++ logout-function.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+
+    if($_SERVER['REQUEST_METHOD']=="POST"){
+            unset($_SESSION['id']);
+            session_destroy();
+            header('location: http://yoursite.index.com');
+        }
+?>
